e0b1cadb36ab6ed2fe36f8d8e971d2e8ea0b98b8
mzw86020424 <mzw86020424@gmail.com>
2023-02-10 09:23:37 +0900

    users/indexをざっくり実装

---
+++ pages/user/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{ users }}
+  </div>
+</template>
+
+<script setup>
+const users = ref([])
+
+onMounted(async() => {
+  users.value = await useFetch('https://jsonplaceholder.typicode.com/users')
+})
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
