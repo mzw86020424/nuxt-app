f385333218f38740ccb45972c703dc88cc0cd493
mzw86020424 <mzw86020424@gmail.com>
2023-02-13 09:10:24 +0900

    名前検索をざっくり実装

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
-    <form @submit.prevent="fetchUsers">
+    <form @submit.prevent="onSubmitId($event)">
       <div>
-        <input v-model="params" />
+        <input v-model="id" />
       </div>
       <div>
         <button type="submit" class="search-button">id検索</button>
+      </div>
+    </form>
+    <form @submit.prevent="onSubmitName($event)">
+      <div>
+        <input v-model="name" />
+      </div>
+      <div>
+        <button type="submit" class="search-button">name検索</button>
       </div>
     </form>
   </div>
@@ -18,16 +26,25 @@
 
 <script setup>
 const users = ref([])
-const params = ref({})
+const id = ref(null)
+const name = ref(null)
 
 onMounted(() => {
   fetchUsers()
 })
 
-const fetchUsers = async () => {
-  const res = await apiGet('/users', {id: params.value});
+const fetchUsers = async (params="") => {
+  const res = await apiGet('/users', params);
   users.value = res;
 };
+
+const onSubmitId = (id) => {
+  fetchUsers({id:id})
+}
+
+const onSubmitName = (name) => {
+  fetchUsers({name:name})
+}
 
 </script>
 
