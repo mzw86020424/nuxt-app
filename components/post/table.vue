5e7352d5de43d7230d899c5b72156bcedb1595ba
mzw86020424 <mzw86020424@gmail.com>
2023-02-15 09:10:46 +0900

    post一覧を実装

---
+++ components/post/table.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th>userId</th>
+          <th>title</th>
+          <th>body</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="post in posts" :key="post.id">
+          <td>{{ post.userId }}</td>
+          <td>{{ post.title }}</td>
+          <td>{{ post.body }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  posts: { default: [] }
+})
+
+</script>
+
+<style lang="scss" scoped>
+  table {
+    width: 100%;
+    border-collapse: collapse;
+  }
+  th, td {
+    border: 1px solid #4CAF50;
+    padding: 8px;
+    text-align: left;
+  }
+  th {
+    background-color: #4CAF50;
+  }
+</style>
---
+++ pages/post/index.vue
@@ -0,0 +1,67 @@
+<template>
+  <!-- <div>
+    <form @submit.prevent="onSubmitId()">
+      <div>
+        <input v-model="id" />
+      </div>
+      <div>
+        <button type="submit" class="search-button">id検索</button>
+      </div>
+    </form>
+    <form @submit.prevent="onSubmitName()">
+      <div>
+        <input v-model="name" />
+      </div>
+      <div>
+        <button type="submit" class="search-button">name検索</button>
+      </div>
+    </form>
+  </div> -->
+  <div>
+    <post-table
+      :posts="posts"
+    />
+  </div>
+</template>
+
+<script setup>
+const posts = ref([])
+// const id = ref(null)
+// const name = ref(null)
+
+onMounted(() => {
+  fetchPosts()
+})
+
+const fetchPosts = async (params="") => {
+  const res = await apiGet('/posts', params);
+  posts.value = res;
+};
+
+// const onSubmitId = () => {
+//   const param = id.value == "" ? null : {id: id.value};
+//   fetchUsers(param)
+// }
+
+// const onSubmitName = () => {
+//   const param = name.value == "" ? null : {name: name.value};
+//   fetchUsers(param)
+// }
+
+</script>
+
+<style lang="scss" scoped>
+.search-button {
+  padding: 10px 20px;
+  background-color: #4CAF50;
+  color: white;
+  border-radius: 5px;
+  border: none;
+  outline: none;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #3e8e41;
+  }
+}
+</style>
