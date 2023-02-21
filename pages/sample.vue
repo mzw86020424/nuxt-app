3d0666345427c7d7a43e36011da276c32d0c1ec7
mzw86020424 <mzw86020424@gmail.com>
2023-02-21 09:18:55 +0900

    googlemapapi実装途中

---
+++ pages/sample.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>テスト用ページ</h1>
+    <div id="map"></div>
+  </div>
+</template>
+
+<script setup>
+const map = ref(null)
+
+loader.load().then(() => {
+  map.value = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+});
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
