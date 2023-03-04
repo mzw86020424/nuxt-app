70691d9b17b23af8e00f9de47184897102a0f7a7
mzw86020424 <mzw86020424@gmail.com>
2023-03-04 10:30:37 +0900

    ヘッダーをコンポーネントに切り出し

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,65 +1,6 @@
 <template>
   <div>
-    <header>
-      <div class="logo">
-        rakuten
-      </div>
-      <nav>
-        <ul>
-          <li>
-            <nuxt-link to="/user">
-              user
-            </nuxt-link>
-          </li>
-          <li>
-            <nuxt-link to="/post">
-              post
-            </nuxt-link>
-          </li>
-          <li>
-            <nuxt-link to="/rakuten/item">
-              rakuten
-            </nuxt-link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <generals-header />
     <slot />
   </div>
 </template>
-
-<script setup>
-
-</script>
-
-<style lang="scss" scoped>
-  header {
-      background-color: #4CAF50;
-      color: #fff;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      padding: 1rem;
-    }
-
-    .logo {
-      font-size: 1.5rem;
-      font-weight: bold;
-    }
-
-    ul {
-      display: flex;
-      gap: 1rem;
-      padding: 1rem;
-    }
-
-    li {
-      list-style: none;
-    }
-
-    a {
-      color: #fff;
-      text-decoration: none;
-    }
-
-</style>
