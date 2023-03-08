4740cb6995cf8a01beb4c5b655c863534859a901
mzw86020424 <mzw86020424@gmail.com>
2023-03-08 09:19:53 +0900

    vulidateの準備

--- pages/sample.vue
+++ pages/sample.vue
@@ -1,19 +1,60 @@
+<!-- https://reffect.co.jp/vue/vulidate-2 -->
 <template>
   <div>
-    <h1>テスト用ページ</h1>
-    <div id="map" />
+    <div>
+      <h1>入力フォームバリデーション</h1>
+      <form @submit.prevent="submitForm">
+        <div>
+          <label for="name"> 名前 </label>
+          <input
+            id="name"
+            v-model="formData.name"
+            type="text"
+            placeholder="名前"
+          >
+        </div>
+        <div>
+          <label for="age">年齢 </label>
+          <input
+            id="age"
+            v-model="formData.age"
+            type="age"
+            placeholder="年齢"
+          >
+        </div>
+        <div>
+          <label for="email">
+            メールアドレス
+          </label>
+          <input
+            id="email"
+            v-model="formData.email"
+            type="email"
+            placeholder="メールアドレス"
+          >
+        </div>
+        <div>
+          <button
+            type="submit"
+          >
+            送信
+          </button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
 <script setup>
-const map = ref(null)
-
-loader.load().then(() => {
-  map.value = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8
-  })
+const formData = reactive({
+  name: '',
+  age: '',
+  email: ''
 })
+
+const submitForm = () => {
+  console.log('submit', formData)
+}
 
 </script>
 
