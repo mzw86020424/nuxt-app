66ca68fe5fdd704f92348bbde7f70b11feb0ad19
mzw86020424 <mzw86020424@gmail.com>
2023-03-08 09:09:30 +0900

    areaの中身の準備

---
+++ pages/rakuten/travel/area.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    area
+  </div>
+</template>
+
+<script setup>
+const rakutenAreaCodes = ref({})
+const params = ref({})
+
+onMounted(async () => {
+  await fetchRatekunAreaCodes()
+})
+
+const fetchRatekunAreaCodes = async () => {
+  const res = await rakutenApiGet('/Travel/GetAreaClass/20131024?format=json', params)
+  rakutenAreaCodes.value = res.Items
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
