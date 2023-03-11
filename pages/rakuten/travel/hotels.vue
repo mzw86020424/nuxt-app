6c5e57097c708630a6b463b8fe92de16722f1fe6
mzw86020424 <mzw86020424@gmail.com>
2023-03-11 20:49:05 +0900

    ホテル検索を仮実装

---
+++ pages/rakuten/travel/hotels.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div>
+      <form @submit.prevent="searchHotels">
+        <div>
+          <label for="name"> ホテル検索 </label>
+          <input v-model="formData.keyword" type="text">
+          <button
+            type="submit"
+          >
+            検索
+          </button>
+        </div>
+      </form>
+    </div>
+    <div>
+      {{ hotels }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+const formData = reactive({
+  keyword: ''
+})
+const hotels = ref([])
+const page = ref(1)
+const hits = ref(2)
+
+const searchHotels = async () => {
+  const params = {
+    keyword: utf8Encode(formData.keyword),
+    page: page.value,
+    hits: hits.value
+  }
+  const res = await rakutenApiGet('/Travel/KeywordHotelSearch/20170426?format=json', params)
+  hotels.value = res.hotels
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
